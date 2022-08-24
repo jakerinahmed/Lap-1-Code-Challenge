9607a7b8f1fae2edce620da349077310c8835db0
ultimafinal1 <matthewblackman123@gmail.com>
2022-08-24 16:52:58 +0100

    results page + links to external pages

--- client/script.js
+++ client/script.js
@@ -1,19 +1,28 @@
-const searchResults = document.getElementById('searchEngine');
-const searchBar = document.getElementById('searchBar');
+const searchResults = document.getElementById('searchResults')
+const ul = document.querySelector('ul');
 
-searchResults.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const searchQuery = searchBar.value;
-    Promise.all([
-        fetch('http://localhost:3000/movies'),
-        fetch('http://localhost:3000/tv-shows'),
-        fetch('http://localhost:3000/video-games') 
-    ]).then((responses) => Promise.all(responses.map(res => res.json())))
-    .then((data) => data.forEach(obj => {
-        obj.forEach(arr => {
-            if ((arr.title).includes(searchQuery)) {
-                console.log(arr);
-            }
-        });
-    }));
-})
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const search = urlParams.get('search');
+console.log(search);
+
+Promise.all([
+    fetch('http://localhost:3000/movies'),
+    fetch('http://localhost:3000/tv-shows'),
+    fetch('http://localhost:3000/video-games') 
+]).then((responses) => Promise.all(responses.map(res => res.json())))
+.then((data) => data.forEach(obj => {
+    obj.forEach(arr => {
+        if ((arr.title).includes(search)) {
+            console.log(arr);
+            const newA = document.createElement('a');
+   
+            newA.setAttribute('href', arr.link);
+            ul.appendChild(newA);
+            const newLi = document.createElement('li');
+            
+            newLi.textContent = arr.title;
+            newA.appendChild(newLi);
+        }
+    });
+}));
